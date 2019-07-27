70945aa8310bfe74fc0a2a01a99464bbe3d7dfdf
David New <46327493+davidanew@users.noreply.github.com>
2019-07-27 14:47:23 +0100

    Removed voice_parameters

--- Voice.cpp
+++ Voice.cpp
@@ -5,11 +5,11 @@ Voice::Voice(const Global_parameters& global_parameters, const uint32_t& freq, c
 	: global_parameters(global_parameters)
 {
 	//TODO: These frequencies need to be seperate or have offsets
-	voice_parameters.freq_1 = freq;
-	voice_parameters.freq_2 = freq;
-	voice_parameters.velocity = velocity_arg;
-	voice_parameters.phase_rel_per_tick_1 = voice_parameters.freq_1 / global_parameters.sample_tick_freq;
-	voice_parameters.phase_rel_per_tick_2 = voice_parameters.freq_2 / global_parameters.sample_tick_freq;
+	freq_1 = freq;
+	freq_2 = freq;
+	velocity = velocity_arg;
+	phase_rel_per_tick_1 = freq_1 / global_parameters.sample_tick_freq;
+	phase_rel_per_tick_2 = freq_2 / global_parameters.sample_tick_freq;
 	try {
 		//TODO: These will also need to be set with dial
 		//make sure deconstructed properly when changed
@@ -45,28 +45,28 @@ Voice::Voice(const Global_parameters& global_parameters, const uint32_t& freq, c
 float Voice::update_and_get_sample(const uint64_t sample_tick) {
 	//coded to prevent costly int to float conversion and fmod
 	//allows recovery from missed sample ticks
-	if(voice_parameters.previous_sample_tick == 0) {
+	if(previous_sample_tick == 0) {
 		//Set this so there is not a long delay due to the first sample
-		voice_parameters.previous_sample_tick = sample_tick;
+		previous_sample_tick = sample_tick;
 	}
 	//Get to current sample tick,
 	//updating phase each time
 	//current phase being considered is held in
 	//phase_rel_*
-	while(sample_tick > voice_parameters.previous_sample_tick) {
-		voice_parameters.phase_rel_1 += voice_parameters.phase_rel_per_tick_1;
-		if (voice_parameters.phase_rel_1 > (float) 1.0) 
-			voice_parameters.phase_rel_1 -= 1;
-		voice_parameters.phase_rel_2 += voice_parameters.phase_rel_per_tick_2;
-		if (voice_parameters.phase_rel_2 > (float) 1.0) 
-			voice_parameters.phase_rel_2 -= 1;
-		voice_parameters.previous_sample_tick += 1;
+	while(sample_tick > previous_sample_tick) {
+		phase_rel_1 += phase_rel_per_tick_1;
+		if (phase_rel_1 > (float) 1.0) 
+			phase_rel_1 -= 1;
+		phase_rel_2 += phase_rel_per_tick_2;
+		if (phase_rel_2 > (float) 1.0) 
+			phase_rel_2 -= 1;
+		previous_sample_tick += 1;
 	}
 	//Previous_sample_tick will now be set to the current sample tick
 	//The function should not be called until the next tick, but it doesn't matter if
 	//it is called earlier
 	//Calculate the sample from the two waveforms and return value
-	return (float)(wave_1->get_value(voice_parameters.phase_rel_1) * global_parameters.ampl_1 + 
-					wave_2->get_value(voice_parameters.phase_rel_2) * global_parameters.ampl_2)
-	                * voice_parameters.velocity;
+	return (float)(wave_1->get_value(phase_rel_1) * global_parameters.ampl_1 + 
+					wave_2->get_value(phase_rel_2) * global_parameters.ampl_2)
+	                * velocity;
 }	
